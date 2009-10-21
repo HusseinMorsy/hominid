32d882830cadea30c227ca0980ed5c5b8963234f
Michael Strüder <mike@mike-desktop.(none)>
2009-10-21 11:11:54 +0200

    rake task implemented

--- lib/hominid.rb
+++ lib/hominid.rb
@@ -1,40 +1,51 @@
 require 'xmlrpc/client'
- 
+
 module Hominid
- 
+
   class HominidError < StandardError
     def initialize(error)
       super("#{error.message}")
     end
   end
- 
+
   class HominidListError < HominidError
   end
- 
+
   class HominidListEmailError < HominidListError
   end
- 
+
   class HominidListMergeError < HominidListError
   end
- 
+
   class AlreadySubscribed < HominidListEmailError
   end
- 
+
   class AlreadyUnsubscribed < HominidListEmailError
   end
- 
+
   class NotExists < HominidListEmailError
   end
- 
+
   class NotSubscribed < HominidListEmailError
   end
- 
+
   class HominidCommunicationError < HominidError
     def initialize(message)
       super(message)
     end
   end
- 
+
 end
- 
+
+begin
+  # inlcude the provided rake task
+  require 'rake'
+  unless Rake::Task.task_defined? "hominid:config"
+    load File.join(File.dirname(__FILE__), '..', 'tasks', 'rails', 'hominid.rake')
+  end
+rescue LoadError
+  # silently skip rake task inclusion unless the rake gem is installed
+end
+
 require 'hominid/base'
+
--- tasks/rails/hominid.rake
+++ tasks/rails/hominid.rake
@@ -1,12 +1,22 @@
-require 'ftools'
-
+require 'rake'
 namespace :hominid do
   # Task to create the config file
   desc "Generate a Hominid config file"
   task :config => :environment do |t|
+    require 'fileutils'
     if defined?(Rails.root)
-      ## TODO: copy config template to Rails project
-      # File.copy("hominid.yml.tpl", "#{Rails.root}/config/hominid.yml")
+      config_file = File.join(Rails.root, 'config', 'hominid.yml')
+      template_file = File.join(File.dirname(__FILE__), '..', '..', 'hominid.yml.tpl')
+      unless File.exists? config_file
+        FileUtils.cp(
+          File.join(File.dirname(__FILE__), '..', '..', 'hominid.yml.tpl'),
+          File.join(Rails.root, 'config', 'hominid.yml')
+        )
+        puts 'Please edit config/hominid.yml to your needs.'
+      else
+        puts 'We left your existing config/hominid.yml untouched.'
+      end
     end
   end
 end
+
