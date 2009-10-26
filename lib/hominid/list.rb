b9fec13f2324da77e17f4160e617e76918fa78f8
Michael Strüder <mike@mike-desktop.(none)>
2009-10-26 12:58:18 +0100

    added aliases to match the Mailchimp API method names

--- lib/hominid/list.rb
+++ lib/hominid/list.rb
@@ -47,26 +47,32 @@ module Hominid
       # Add an interest group to a list
       call("listInterestGroupAdd", @list_id, group)
     end
+    alias :interest_group_add :create_group
 
     def create_tag(tag, name, required = false)
       # Add a merge tag to a list
       call("listMergeVarAdd", @list_id, tag, name, required)
     end
+    alias :merge_var_add :create_tag
 
     def delete_group(group)
       # Delete an interest group for a list
       call("listInterestGroupDel", @list_id, group)
     end
+    alias :interest_group_del :delete_group
 
     def delete_tag(tag)
       # Delete a merge tag and all its members
       call("listMergeVarDel", @list_id, tag)
     end
+    alias :merge_var_del :delete_tag
 
     def groups()
       # Get the interest groups for a list
       call("listInterestGroups", @list_id)
     end
+    alias :interest_groups :groups
+
 
     def member_info(email)
       # Get a member of a list
@@ -90,6 +96,7 @@ module Hominid
       # Get the merge tags for a list
       call("listMergeVars", @list_id)
     end
+    alias :merge_vars :merge_tags
 
     def subscribe(email, options = {})
       # Subscribe a member to this list
@@ -115,6 +122,7 @@ module Hominid
       options = apply_defaults_to({:update_existing => true}.merge(options))
       call("listBatchSubscribe", @list_id, subscribers, *options.values_at(:double_opt_in, :update_existing, :replace_interests))
     end
+    alias :batch_subscribe :subscribe_many
 
     def unsubscribe(current_email, options = {})
       # Unsubscribe a list member
@@ -128,6 +136,7 @@ module Hominid
       options = apply_defaults_to({:delete_member => true}.merge(options))
       call("listBatchUnsubscribe", @list_id, emails, *options.values_at(:delete_member, :send_goodbye, :send_notify))
     end
+    alias :batch_unsubscribe :unsubscribe_many
 
     def update_member(current_email, merge_tags = {}, email_type = "html")
       # Update a member of this list
