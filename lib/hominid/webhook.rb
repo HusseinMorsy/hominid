aeeab6f94b4e2dd865f21b662a27496155e26555
bgetting <brian@terra-firma-design.com>
2009-10-30 11:50:04 -0700

    Finished Hominid#Webhook Class

--- lib/hominid/webhook.rb
+++ lib/hominid/webhook.rb
@@ -25,6 +25,10 @@ module Hominid
     # h.first_name          <= (String)     The first name of the subscriber (if available).
     # h.last_name           <= (String)     The first name of the subscriber (if available).
     # h.interests           <= (Array)      An array of the interest groups.
+    # h.ip_opt              <= (String)     The opt in IP address.
+    # h.ip_signup           <= (String)     The signup IP address.
+    #
+    # {"type" => "subscribe", "fired_at" => "2009-03-26 21:35:57", "data" => {"id" => "8a25ff1d98", "list_id" => "8a25ff1d98", "email" => "api@mailchimp.com", "email_type" => "html", "merges" => {"EMAIL" => "api@mailchimp.com", "FNAME" => "Brian", "LNAME" => "Getting", "INTERESTS" => "Group1,Group2"}, "ip_opt" => "10.20.10.30", "ip_signup" => "10.20.10.30"}}
     
     attr_reader :request
     
@@ -35,20 +39,21 @@ module Hominid
       @request = hash_to_object(post_data)
     end
     
+    
+    def email
+      self.request.data.email if self.request.data.email
+    end
+    
+    def email_type
+      self.request.data.email_type if self.request.data.email_type
+    end
+    
     def event
       self.request.event if self.request.event
     end
     
     def fired_at
       self.request.fired_at.to_datetime if self.request.fired_at
-    end
-    
-    def id
-      self.request.data.id if self.request.data.id
-    end
-    
-    def list_id
-      self.request.data.list_id if self.request.data.list_id
     end
     
     def first_name
@@ -59,8 +64,36 @@ module Hominid
       self.request.data.merges.lname if self.request.data.merges.lname
     end
     
+    def id
+      self.request.data.id if self.request.data.id
+    end
+    
     def interests
       self.request.data.merges.interests.split(',') if self.request.data.merges.interests
+    end
+    
+    def ip_opt
+      self.request.data.ip_opt if self.request.data.ip_opt
+    end
+    
+    def ip_signup
+      self.request.data.ip_signup if self.request.data.ip_signup
+    end
+    
+    def list_id
+      self.request.data.list_id if self.request.data.list_id
+    end
+    
+    def new_email
+      self.request.data.new_email if self.request.data.new_email
+    end
+    
+    def old_email
+      self.request.data.old_email if self.request.data.old_email
+    end
+    
+    def reason
+      self.request.data.reason if self.request.data.reason
     end
     
     private
@@ -71,7 +104,6 @@ module Hominid
         object = object.clone
         object.each do |key, value|
           object[key.downcase] = hash_to_object(value)
-          # TODO: Replace keys with lowercase, rather than duplicating
         end
         OpenStruct.new(object)
       when Array
